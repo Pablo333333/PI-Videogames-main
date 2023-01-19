87b678b1ae600658099df06486a2214d97460f40
Pablo333333 <pj669349@gmail.com>
2023-01-19 13:17:20 -0300

    pi finish

--- api/index.js
+++ api/index.js
@@ -19,55 +19,11 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const axios= require('axios')
-const {API_KEY}= process.env;
-const apiKey = `?key=${API_KEY}`
-
 // Syncing all the models at once.
-
-async function getVideogames(){
-  const apiUrl = await axios.get(`https://api.rawg.io/api/games${apiKey}`);
-	var next = apiUrl.data.next;
-	var apiVideogames= apiUrl.data.results;
-
-		for (let i = 1; i < 5; i++) {
-		let element = await axios.get(next);
-		element.data.results.map(el => apiVideogames.push(el))
-		next = element.data.next;
-	}
-
-	apiVideogames = apiVideogames.map(el => {
-			let games = {	
-				id: el.id,
-				name: el.name,
-				img: el.background_image,	
-				rating: el.rating,
-				platforms: el.parent_platforms.map(el => el.platform.name),
-				genres: el.genres
-			};
-			console.log(games);
-		});	
-}
-
-async function getGenres(){
-  const infoApi = await axios.get(`https://api.rawg.io/api/genres${apiKey}`);
-	const apiJson = infoApi.data.results.map(el => {
-		return{
-			id: el.id,
-			name: el.name
-		}
-	})
-	console.log(apiJson);
-}
-
-
 
 
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
-    getVideogames()
-    getGenres()
-    
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(process.env.PORT, () => {
+    console.log(`Server listening at ${process.env.PORT}`); // eslint-disable-line no-console
   });
 });
